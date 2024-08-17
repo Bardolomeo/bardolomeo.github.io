1d478f79d7e12796ecbd46e1b9cfab0e6716f01f
Bardolomeo <sapiobard@gmail.com>
2024-08-17 11:34:46 +0200

    add actions

--- fabula-sheets/next.config.mjs
+++ fabula-sheets/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath : "/fabula-sheets",
+	output : "export",
+	reactStrictMode: true
+};
 
 export default nextConfig;
